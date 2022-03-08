1f092bafd00a1b077231f84cb8f26b9369d04bd9
jeetsimrannnn <s99chd@hotmail.com>
2022-03-08 15:43:14 -0500

    541

--- orderdetails.php
+++ orderdetails.php
@@ -1,13 +1,25 @@
-function get_nama(){
-    $kota = $this->input->get('kota');
+<?php
+    include "dbconnect.php";
 
-    $query = $this->db->query("SELECT id, nama FROM tb_sekolah WHERE kota = '{$kota}'");
+    $orderno = $_REQUEST['ordernos'];
+    if ($orderno !== "") {
+        
+        // Get corresponding first name and 
+        // last name for that user id    
+        $sqlorder = "SELECT CustomerName FROM tblCustOrders INNER JOIN tblCustomers ON tblCustOrders.CustID = tblCustomers.CustID WHERE OrderID = '".$orderno."'";
+        $result = sqlsrv_query($conn,$sqlorder) or die("Couldn't execut query");
+        $data = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
 
-    $data = array(
-        'nama' => $query->result_array(),
-        'success' => TRUE
-    );
-
-    print json_encode($data);
-
-}
+        // Get the first name
+        // $order_no = $data["OrderNo"];
+    
+        // Get the first name
+        $cust_name = $data["CustomerName"];
+    }
+    // Store it in a array
+    $resultarr = array("$cust_name");
+    
+    // Send in JSON encoded form
+    $myJSON = json_encode($resultarr);
+    echo $myJSON;
+?>
--- servicereports.php
+++ servicereports.php
@@ -131,7 +131,7 @@
         <div class="form-group mb-3 inputfield">
             <label for="orderno">Order Number</label>
             <!-- <input type="text" class="form-control" id="OrderNumber" name="OrderNumber" placeholder="Enter Order Number"   /> -->
-            <input list="orderno" name="ordernos" id="ordernos" class="form-control" placeholder="Enter Order Number..."/>
+            <input list="orderno" name="ordernos" id="ordernos" class="form-control" onkeyup="GetDetail(this.value)" placeholder="Enter Order Number..."/>
             <datalist id="orderno">
              <?php
              $serverName = 'tcp:teamoffline.database.windows.net,1433';
@@ -431,4 +431,48 @@ $(document).ready(function(){
         //     });
         });
     });
+</script>
+
+<script>
+  
+  // onkeyup event will occur when the user 
+  // release the key and calls the function
+  // assigned to this event
+  function GetDetail(str) {
+      if (str.length == 0) {
+          document.getElementById("ordernos").value = "";
+          return;
+      }
+      else {
+
+          // Creates a new XMLHttpRequest object
+          var xmlhttp = new XMLHttpRequest();
+          xmlhttp.onreadystatechange = function () {
+
+              // Defines a function to be called when
+              // the readyState property changes
+              if (this.readyState == 4 && 
+                      this.status == 200) {
+                    
+                  // Typical action to be performed
+                  // when the document is ready
+                  var myObj = JSON.parse(this.responseText);
+
+                  // Returns the response data as a
+                  // string and store this array in
+                  // a variable assign the value 
+                  // received to first name input field
+                    
+                  document.getElementById
+                      ("travelto").value = myObj[0];
+              }
+          };
+
+          // xhttp.open("GET", "filename", true);
+          xmlhttp.open("GET", "orderdetails.php?orderno=" + str, true);
+            
+          // Sends the request to the server
+          xmlhttp.send();
+      }
+  }
 </script>
