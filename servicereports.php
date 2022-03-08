6f5996e99b18633ce0065a13d9b2f90a1fcb59e7
jeetsimrannnn <s99chd@hotmail.com>
2022-03-08 13:02:57 -0500

    btg

--- servicereports.php
+++ servicereports.php
@@ -385,7 +385,7 @@ $(document).ready(function(){
     if (month < 10) month = "0" + month;
     if (day < 10) day = "0" + day;
 
-    var today = year + "-" + month + "-" + day +"T00:00";       
+    var today = year + "-" + month + "-" + day;       
     $("#servicedate").attr("value", today);
     });
 </script>
