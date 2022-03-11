5b10bc494bb6c2f0778ed56a7c148810c1f4c322
jeetsimrannnn <s99chd@hotmail.com>
2022-03-11 10:45:29 -0500

    sdv 3

--- root/sp_newSR.php
+++ root/sp_newSR.php
@@ -5,27 +5,9 @@
         $tsql_callSP = "{call sp_tblService_NewItem(?)};";
 
         $EmployeeID = 0;
-        $TravelFrom = "";
-        $MileageAllowance = 0;
-        $MileageAllowanceBillable = 0;
-        $USExchange = 0;
-        $ServiceDate = '2022-03-10 20:44:27.510';
-        $MileageBillable = 0;
-        $Processed = 0;
-        $Submitted = 0;
-        $Reviewed = 0;
 
         $params = array( 
             array($EmployeeID, SQLSRV_PARAM_IN)
-            // ,array(&$TravelFrom, SQLSRV_PARAM_OUT),
-            // array(&$MileageAllowance, SQLSRV_PARAM_OUT),
-            // array(&$MileageAllowanceBillable, SQLSRV_PARAM_OUT),
-            // array(&$USExchange, SQLSRV_PARAM_OUT),
-            // array(&$ServiceDate, SQLSRV_PARAM_OUT),
-            // array(&$MileageBillable, SQLSRV_PARAM_OUT),
-            // array(&$Processed, SQLSRV_PARAM_OUT),
-            // array(&$Submitted, SQLSRV_PARAM_OUT),
-            // array(&$Reviewed, SQLSRV_PARAM_OUT)
           );
 
         $stmtNewSR = sqlsrv_query( $conn, $tsql_callSP, $params);  
@@ -41,20 +23,34 @@
             die( print_r( sqlsrv_errors(), true));
         }
 
-        $name = sqlsrv_get_field( $stmtNewSR, 0);
-        echo "$name: ".$name;
-
-        // sqlsrv_next_result($stmtNewSR); 
+        $TravelFrom = sqlsrv_get_field( $stmtNewSR, 0);
+        $MileageAllowance= sqlsrv_get_field( $stmtNewSR, 1);
+        $MileageAllowanceBillable= sqlsrv_get_field( $stmtNewSR, 2);
+        $USExchange= sqlsrv_get_field( $stmtNewSR, 3);
+        $ServiceDate= sqlsrv_get_field( $stmtNewSR, 4);
+        $MileageBillable= sqlsrv_get_field( $stmtNewSR, 5);
+        $Processed= sqlsrv_get_field( $stmtNewSR, 6);
+        $Submitted= sqlsrv_get_field( $stmtNewSR, 7);
+        $Reviewed= sqlsrv_get_field( $stmtNewSR, 8);
         
-        // echo $TravelFrom;
-        // echo $MileageAllowance;
-        // echo $MileageAllowanceBillable;
-        // echo $USExchange;
-        // echo $ServiceDate;
-        // echo $MileageBillable;
-        // echo $Processed;
-        // echo $Submitted;
-        // echo $Reviewed;
+        
+        echo $TravelFrom;
+        echo "\n" ;
+        echo $MileageAllowance;
+        echo "\n" ;
+        echo $MileageAllowanceBillable;
+        echo "\n" ;
+        echo $USExchange;
+        echo "\n" ;
+        echo $ServiceDate;
+        echo "\n" ;
+        echo $MileageBillable;
+        echo "\n" ;
+        echo $Processed;
+        echo "\n" ;
+        echo $Submitted;
+        echo "\n" ;
+        echo $Reviewed;
 
         sqlsrv_free_stmt( $stmtNewSR);
         sqlsrv_close( $conn);
