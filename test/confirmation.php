0dcc506c33762988dc140f5092cef370216b9b6f
jeetsimrannnn <s99chd@hotmail.com>
2022-03-15 16:06:37 -0400

    302

--- test/confirmation.php
+++ test/confirmation.php
@@ -7,6 +7,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!empty($name) && !empty($comment)) {
         echo "<p>Hi, <b>$name</b>. Your comment has been received successfully.<p>";
         echo "<p>Here's the comment that you've entered: <b>$comment</b></p>";
+
+                /* Connect to the local server using Windows Authentication and
+                specify the AdventureWorks database as the database in use. */
+
+                $serverName = "tcp:teamoffline.database.windows.net,1433";  
+                $connectionOptions = array(  
+                    "Database" => "TEAMOffline",  
+                    "UID" => "sim1999",  
+                    "PWD" => "simran@99"  
+                );  
+                $conn = sqlsrv_connect($serverName, $connectionOptions);  
+                if( $conn === false )
+                {
+                    echo "Could not connect.\n";
+                    die( print_r( sqlsrv_errors(), true));
+                }
+
+                /* Define the batch query. */
+                $tsql = "INSERT INTO tblService (EmployeeID) VALUES (?);";
+
+                /* Assign parameter values and execute the query. */
+                $params = array($name);
+                $stmt = sqlsrv_query($conn, $tsql, $params);
+                if( $stmt === false )
+                {
+                    echo "Error in statement execution.\n";
+                    die( print_r( sqlsrv_errors(), true));
+                }
+
     } else {
         echo "<p>Please fill all the fields in the form!</p>";
     }
