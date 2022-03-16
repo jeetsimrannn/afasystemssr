e10d50242ce795709bd445b577a142e9f2377cf0
jeetsimrannnn <s99chd@hotmail.com>
2022-03-16 13:31:13 -0400

    614

--- validateLogin.php
+++ validateLogin.php
@@ -74,6 +74,7 @@ if(isset($_POST['login']))
     $custname = sqlsrv_get_field( $stmt, 0);
     echo "
       <script>
+      setCookie('EmployeeID',<?php echo $CustName; ?>,7);
       $(window).on('load', function() {
         window.location='datatables/tblService.php'; 
         function modalfunction(){ $('#preloader').modal('show')};
@@ -140,6 +141,4 @@ if(isset($_POST['login']))
   // header("location:servicereports.php");
 }
 ?>
-<script>
-  setCookie('EmployeeID',<?php echo $CustName; ?>,7);
-</script>
+  
