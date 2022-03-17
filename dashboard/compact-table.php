fd527f74be343405dab2b0e4bef68de56443c109
jeetsimrannnn <s99chd@hotmail.com>
2022-03-17 15:20:46 -0400

    asc

--- dashboard/compact-table.php
+++ dashboard/compact-table.php
@@ -10,17 +10,17 @@
 
   <title>Fresh Bootstrap Table by Creative Tim</title>
 
-  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+  <!-- <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css">
   <link href="assets/css/fresh-bootstrap-table.css" rel="stylesheet" />
-  <link href="assets/css/demo.css" rel="stylesheet" />
+  <link href="assets/css/demo.css" rel="stylesheet" /> -->
 
   <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> -->
   <!-- <link href="http://fonts.googleapis.com/css?family=Roboto:400,700,300" rel="stylesheet" type="text/css"> -->
 
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
   <!-- <script src="https://unpkg.com/bootstrap-table/dist/bootstrap-table.min.js"></script> -->
 
 
