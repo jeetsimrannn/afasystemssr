cd5f41677d9eafc0232d2eb6842565ac7e8284e9
jeetsimrannnn <s99chd@hotmail.com>
2022-03-18 14:46:03 -0400

    sdv

--- datatables/tblService.php
+++ datatables/tblService.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION['SRStatus'] = "";
+// $_SESSION['SRStatus'] = "";
 ?>
 <!doctype html>
 <html>
--- root/updateReportSession.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-
-    $_SESSION['SRStatus'] = 1;
-    echo $_SESSION['SRStatus'];
-?>
