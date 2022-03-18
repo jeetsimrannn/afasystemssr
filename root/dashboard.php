e28aca16ef71db28f942ff6e9e7bf0a1dbcc799c
jeetsimrannnn <s99chd@hotmail.com>
2022-03-18 09:58:38 -0400

    362

--- root/dashboard.php
+++ root/dashboard.php
@@ -12,17 +12,24 @@
 
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css">
-  <link href="assets/css/fresh-bootstrap-table.css" rel="stylesheet" />
-  <link href="assets/css/demo.css" rel="stylesheet" />
-
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-  <!-- <link href="http://fonts.googleapis.com/css?family=Roboto:400,700,300" rel="stylesheet" type="text/css"> -->
-
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="https://unpkg.com/bootstrap-table/dist/bootstrap-table.min.js"></script>
-
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.2/css/fixedHeader.bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css">
+  
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/fixedheader/3.2.2/js/dataTables.fixedHeader.min.js"></script>
+  <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+  <script src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap.min.js"></script>
 
 </head>
 <body>
@@ -51,42 +58,46 @@
             <button id="alertBtn" class="btn btn-default">Alert</button>
           </div>
 
-          <table id="fresh-table" class="table">
+          <table id="example" class="table table-striped table-bordered nowrap" style="width:100%">
             <thead>
               <th data-field="serviceid" data-sortable="true">Service ID</th>
               <th data-field="servicedate" data-sortable="true">Service Date</th>
               <th data-field="ordeerno" data-sortable="true">Order No</th>
-              <th data-field="actions" data-formatter="operateFormatter" data-events="operateEvents">Actions</th>
+              <th data-field="customername" data-sortable="true">Order No</th>
+              <!-- <th data-field="actions" data-formatter="operateFormatter" data-events="operateEvents">Actions</th> -->
             </thead>
             <tbody>
             <?php
-                              $serverName = 'tcp:teamoffline.database.windows.net,1433';
-                              $uid = 'sim1999';
-                              $pwd = 'simran@99';
-                              $databaseName = 'TEAMOffline';
-
-                              $connectionInfo = array( 'UID'=>$uid, 
-                                                      'PWD'=>$pwd,
-                                                      'Database'=>$databaseName);
-
-                              $conn = sqlsrv_connect($serverName,$connectionInfo);
-                              if($conn){
-                                  echo '';
-                              }else{
-                                  echo 'Connection failure<br />';
-                              die(print_r(sqlsrv_errors(),TRUE));
-                              }
-                                  $sql00 = "SELECT * FROM dbo.tblService INNER JOIN dbo.tblCustOrders ON dbo.tblService.OrderID = dbo.tblCustOrders.OrderID";
-                                  $result00 = sqlsrv_query($conn,$sql00) or die("Couldn't execut query");
-                                  while ($data00=sqlsrv_fetch_array($result00, SQLSRV_FETCH_ASSOC)){
-                                  echo '<tr>';
-                                  echo '<td>'.$data00['ServiceID'].'</td>';
-                                  echo '<td>'.date_format($data00['ServiceDate'], 'M j Y').'</td>';
-                                  echo '<td>'.$data00['OrderNo'].'</td>';
-                                  echo '<td></td>';
-                                  echo '</tr>' ;
-                              }
-                      ?>
+                $serverName = 'tcp:teamoffline.database.windows.net,1433';
+                $uid = 'sim1999';
+                $pwd = 'simran@99';
+                $databaseName = 'TEAMOffline';
+            
+                $connectionInfo = array( 'UID'=>$uid, 
+                                        'PWD'=>$pwd,
+                                        'Database'=>$databaseName);
+            
+                $conn = sqlsrv_connect($serverName,$connectionInfo);
+                if($conn){
+                    echo '';
+                }else{
+                    echo 'Connection failure<br />';
+                die(print_r(sqlsrv_errors(),TRUE));
+                }
+                    $sql00 = "SELECT * FROM dbo.tblService 
+                              INNER JOIN dbo.tblCustOrders ON dbo.tblService.OrderID = dbo.tblCustOrders.OrderID 
+                              INNER JOIN dbo.tblCustomers ON dbo.tblCustOrders.CustID = dbo.tblCustomers.CustID ";
+                    $result00 = sqlsrv_query($conn,$sql00) or die("Couldn't execut query");
+                    while ($data00=sqlsrv_fetch_array($result00, SQLSRV_FETCH_ASSOC)){
+                    echo '<tr>';
+                    echo '<td>'.$data00['ServiceID'].'</td>';
+                    echo '<td>'.date_format($data00['ServiceDate'], 'M j Y').'</td>';
+                    echo '<td>'.$data00['OrderNo'].'</td>';
+                    echo '<td>'.$data00['CustomerName'].'</td>';
+                    // echo '<td></td>';
+                    echo '</tr>' ;
+                }
+            ?>
             </tbody>
           </table>
         </div>
@@ -176,3 +187,12 @@
   </script>
 
 </html>
+<script>
+  $(document).ready(function() {
+    var table = $('#example').DataTable( {
+        responsive: true
+    } );
+ 
+    new $.fn.dataTable.FixedHeader( table );
+} );
+  </script>
