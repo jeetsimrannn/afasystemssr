9c82f9b0ec3cc57b8fc1330e0c2515768ad5b0e9
jeetsimrannnn <s99chd@hotmail.com>
2022-03-23 14:51:34 -0400

    bdf

--- servicereports.php
+++ servicereports.php
@@ -40,27 +40,7 @@ session_start();
             <div class="form-row row">
                         <div class="col form-group mb-3">
                             <label for="name">Service ID</label>
-                            <input type="text" class="form-control" id="ServiceID" name="ServiceID" placeholder="Enter ID" readonly
-                            value="<?php
-                                include "dbconnect.php";
-                                
-                                $sql = "SELECT max(ServiceID) from tblService";
-                                $stmt = sqlsrv_query( $conn, $sql);
-                                if( $stmt === false ) {
-                                    die( print_r( sqlsrv_errors(), true));
-                                }
-                                
-                                // Make the first (and in this case, only) row of the result set available for reading.
-                                if( sqlsrv_fetch( $stmt ) === false) {
-                                    die( print_r( sqlsrv_errors(), true));
-                                }
-
-                                // Get the row fields. Field indices start at 0 and must be retrieved in order.
-                                // Retrieving row fields by name is not supported by sqlsrv_get_field.
-                                $srvid = sqlsrv_get_field( $stmt, 0);
-                                echo $srvid+1;
-                                ?>"
-                            />
+                            <input type="text" class="form-control" id="ServiceID" name="ServiceID" placeholder="Enter ID" readonly value="0"/>
                         </div> 
                         <div class="col form-group mb-3">
                             <label for="servicedate">Service Date</label>
