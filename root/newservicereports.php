d6a733c16865f3ab9eb6856660a7dea9642ca994
jeetsimrannnn <s99chd@hotmail.com>
2022-03-23 14:07:00 -0400

    dc

--- root/newservicereports.php
+++ root/newservicereports.php
@@ -30,7 +30,7 @@ session_start();
 
 <body>
 
-<?php require 'root/sp_newSR.php'; ?>
+<?php require 'sp_newSR.php'; ?>
 
 <header class="header-transparent" id="header">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light" style="border-bottom:2px solid #0000001a">
