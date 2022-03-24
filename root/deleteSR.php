27818269bbf1f3a7c48b962f476e0408e786d7ab
jeetsimrannnn <s99chd@hotmail.com>
2022-03-24 09:50:09 -0400

    fdv

--- root/deleteSR.php
+++ root/deleteSR.php
@@ -1,12 +1,18 @@
 <?php
      // Check if cookie exists
-    if (!empty($_COOKIE['SRID']))
-    {
-        echo "Cookies are enabled on your browser";
-    } 
-    else 
-    {
-        echo "Cookies are NOT enabled on your browser";
-    }
+    $request = $_REQUEST;
+    $srid = $request['SRID'];
+    
+	include "../dbconnect.php";
+	$sql = "DELETE FROM dbo.tblService WHERE ServiceID ='".$srid."'";
+	$stmt = sqlsrv_query( $conn, $sql);
+	if( $stmt === false ) {
+		die( print_r( sqlsrv_errors(), true));
+	}
+    
+    
+    echo "deleted row".$srid;
+
+
     
 ?>
